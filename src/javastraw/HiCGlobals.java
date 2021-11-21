8d1f2a7509f1acfd2708dbfb495155ec3a433719
Muhammad S Shamim <sa501428@gmail.com>
2021-11-21 02:45:13 -0600

    version

--- src/javastraw/HiCGlobals.java
+++ src/javastraw/HiCGlobals.java
@@ -26,7 +26,7 @@ package javastraw;
 
 public class HiCGlobals {
 
-    public static final String versionNum = "1.06.03";
+    public static final String versionNum = "1.07.00";
 
     // min hic file version supported
     public static final int minVersion = 6;

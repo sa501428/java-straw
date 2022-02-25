3e7ee941a4b458763c6b428d34f86f6776d44c3a
Muhammad S Shamim <sa501428@gmail.com>
2022-02-25 16:26:22 -0600

    update version

--- src/javastraw/StrawGlobals.java
+++ src/javastraw/StrawGlobals.java
@@ -25,7 +25,7 @@
 package javastraw;
 
 public class StrawGlobals {
-    public static final String versionNum = "2.10.01";
+    public static final String versionNum = "2.11.01";
     // min hic file version supported
     public static final int minVersion = 6;
     public static final int bufferSize = 2097152;
